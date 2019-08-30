00d3890cf27cb8a4108d1f8e2364d22ce8408c36
jibril101 <mohsinak49@gmail.com>
2019-08-29 22:41:16 -0700

    Initial application generated by JHipster-6.2.0

---
+++ src/main/java/com/salatracker/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.salatracker.service;
---
+++ src/main/java/com/salatracker/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.salatracker.web.rest.vm;
